dbe423e12844de27561e3827b93aab6b5214cc57
nigeyyy <nigelgambiza27@gmail.com>
2026-06-22 15:47:32 +0400

    add ef core dbcontext and initial migration; create database

---
+++ Supermarket-Management-system.Core/SupermarketContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Supermarket_Management_system.Core
+{
+    public class SupermarketContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier>Suppliers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleItem> SaleItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=SupermarketDb;Trusted_Connection=True;");
+        }
+    }
+}
