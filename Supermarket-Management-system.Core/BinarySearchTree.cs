4ef996f2167903a54c7a32da34c852c879a6fe36
nigeyyy <nigelgambiza27@gmail.com>
2026-06-22 13:55:35 +0400

    added binary search tree with recursive insert

---
+++ Supermarket-Management-system.Core/BinarySearchTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket_Management_system.Core
+{
+    public class BinarySearchTree
+    {
+
+        private TreeNode root;
+
+        public void Insert(string name, string value)
+        {
+            root = InsertHelper(root, name, value);
+        }
+
+        private TreeNode InsertHelper(TreeNode current, string name, string value)
+        {
+
+            if (current == null)
+            {
+                TreeNode newNode = new TreeNode();
+
+                newNode.Name = name;
+
+                newNode.Value = value;
+                return newNode;
+            }
+
+            int compare = string.Compare(name, current.Name);
+
+            if (compare < 0)
+            {
+                current.Left = InsertHelper(current.Left, name, value);
+            }
+            else if (compare > 0)
+            {
+                current.Right = InsertHelper(current.Right, name, value);
+            }
+
+            return current;
+        }
+    }
+}
+
---
+++ Supermarket-Management-system.Core/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket_Management_system.Core;
+
+public class TreeNode
+{
+    public string Name;
+    public string Value;
+    public TreeNode Left;
+    public TreeNode Right;
+
+    }
