54bf32975ef1510039f7944b31d2dd9386a1c8c0
nigeyyy <nigelgambiza27@gmail.com>
2026-06-22 13:27:24 +0400

    added unit tests for the has table:get,missing key,duplicate

---
+++ Supermarket-Management-system.Tests/HashTableTests.cs
@@ -0,0 +1,38 @@
+﻿    using Xunit;
+    using Supermarket_Management_system.Core;
+    using Microsoft.VisualBasic;
+
+    namespace Supermarket_Management_system.Tests;
+
+    public class HashTableTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var table = new HashTable();
+
+            table.Add("12345", "Milk");
+            string result = table.Get("12345");
+
+            Assert.Equal("Milk", result);
+        }
+    [Fact]
+    public void Get_MissingKey_ReturnsNull()
+    {
+        var table = new HashTable();
+
+        string result = table.Get("99999");
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Add_DuplicateKey_ThrowsException()
+    {
+        var table = new HashTable();
+
+        table.Add("12345", "Milk");
+
+        Assert.Throws<Exception>(() => table.Add("12345", "Bread"));
+    }
+}
